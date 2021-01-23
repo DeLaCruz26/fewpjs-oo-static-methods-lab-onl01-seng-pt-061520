4ffdde65ea49ba5eb370b595bcc3718d84595ce0
DeLaCruz26 <dametrik.fick@gmail.com>
2021-01-23 03:33:01 -0500

    commit tree 7e0ca47d2a21e35db034066c7969a70a82848cb8

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static titleize(string){
+    string = string.split(' ')
+    const nonCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    for(let i=0; i < string.length; i++){
+      if(i === 0){
+        string[i] = this.capitalize(string[i])
+      } else {
+        if(!(nonCap.includes(string[i]))){
+          string[i] = this.capitalize(string[i])
+        }
+      }
+    }
+    return string.join(' ')
+  }
 }
